9ab7b87425c82135299145701d54e35bc27b8a37
AndriiPavuk <108342654+AndriiPavuk@users.noreply.github.com>
2023-10-21 17:26:01 +0300

    t3

--- js/task3.js
+++ js/task3.js
@@ -1,4 +1,4 @@
 function checkForSpam(message) {
     message = message.toLowerCase();
-   return message.includes(sale) message.includes(spam);
+    return message.includes(sale) message.includes(spam);
 }
